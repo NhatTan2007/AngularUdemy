61ee7f7430b3aa74fe03c4afeb1525ff5c926477
TAN.TON <nhattan2007@gmail.com>
2021-09-05 22:12:59 +0700

    Update shopping module

--- src/app/shopping/shopping-routing.module.ts
+++ src/app/shopping/shopping-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingComponent } from './shopping.component';
 
 const routes: Routes = [
